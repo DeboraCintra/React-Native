0ab8f7a1e0c59709be85421a31ac7c7ddb0d2929
Debora Cintra <dbrcl.30@gmail.com>
2022-09-09 16:28:53 -0300

    Primeiro commit

---
+++ orgs-cesta/App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import Cesta from './src/telas/Cesta'; 
+
+export default function App(){
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
+  );
+}
