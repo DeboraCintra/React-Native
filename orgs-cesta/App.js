19faba582046b29bb9985d0878c7a09f5ac78d26
Debora Cintra <dbrcl.30@gmail.com>
2022-09-14 13:45:08 -0300

    Curso alura

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,14 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { 
+  useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
 
-import Cesta from './src/telas/Cesta'; 
+import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/Cesta';
 
-export default function App(){
+
+export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if (!fonteCarregada) {
+    return <AppLoading />
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock} />
     </SafeAreaView>
   );
 }
